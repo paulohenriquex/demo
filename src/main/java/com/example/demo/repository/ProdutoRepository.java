6dec7c3c674dc6225e8378bc875b57c7cce78a6d
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-27 19:40:09 -0300

    Organização do código

--- src/main/java/com/example/demo/repository/ProdutoRepository.java
+++ src/main/java/com/example/demo/repository/ProdutoRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import com.example.demo.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto,Long> {
     
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
