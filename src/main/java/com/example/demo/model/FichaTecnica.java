0b014175460a6530d4b854cea59464342a672469
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-27 19:38:01 -0300

    Criação da classe FichaTecnica

--- src/main/java/com/example/demo/model/FichaTecnica.java
+++ src/main/java/com/example/demo/model/FichaTecnica.java
@@ -1,9 +1,18 @@
 package com.example.demo.model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,9 +22,36 @@ import lombok.Setter;
 @Setter
 @Table(name = "ficha_tecnica")
 public class FichaTecnica {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "receita_id")
+    private Receita receita;
+
+    @OneToMany(mappedBy = "fichaTecnica", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ingrediente>ingredientes = new ArrayList<>();
+
+    public BigDecimal getCustoTotal(){
+        BigDecimal total = BigDecimal.ZERO;
+
+        if(ingredientes == null){
+            return BigDecimal.ZERO;
+        }
+        
+        for(Ingrediente ing:ingredientes){
+            total = total.add(ing.getCustoItem());
+        }
+        return total;
+    }
+
+
 
    
 }
