5a3e761ef628d478efbb3115567b7dc68eb801cf
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-27 19:39:13 -0300

    Organização

--- src/main/java/com/example/demo/repository/CategoriaRepository.java
+++ src/main/java/com/example/demo/repository/CategoriaRepository.java
@@ -3,8 +3,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import com.example.demo.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Long>{
    
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
