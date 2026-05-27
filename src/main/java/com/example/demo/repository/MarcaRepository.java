0d9912bd91cf391f0f9d3ba456c726d62f1f2766
paulohenriquex <paulohenriquesouzax@icloud.com>
2026-05-27 19:39:59 -0300

    Organização do codigo

--- src/main/java/com/example/demo/repository/MarcaRepository.java
+++ src/main/java/com/example/demo/repository/MarcaRepository.java
@@ -3,8 +3,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import com.example.demo.model.Marca;
-
-public interface MarcaRepository extends JpaRepository<Marca,Long> {
   
+@Repository
+public interface MarcaRepository extends JpaRepository<Marca, Long> {
 }
